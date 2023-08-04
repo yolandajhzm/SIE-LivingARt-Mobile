5ef7278506fc3c500f520fa2006c0428acee9db1
ruyicS2023 <123344125+ruyicS2023@users.noreply.github.com>
2023-08-03 21:01:02 -0700

    recover ARScreen.js

--- app/screens/ARScreen.js
+++ app/screens/ARScreen.js
@@ -1,10 +1,126 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useRef, useEffect} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {
+  ViroARScene,
+  ViroText,
+  ViroARSceneNavigator,
+  ViroAmbientLight,
+  Viro3DObject,
+  ViroMaterials,
+  ViroNode,
+  ViroARPlaneSelector,
+} from '@viro-community/react-viro';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import HelloWorldSceneAR from './HelloWorldSceneAR';
+import ReticleSceneAR from './ReticleSceneAR';
+import RNFS from 'react-native-fs';
+import unzip from 'react-native-zip-archive';
+
+async function fetchAndUnzip(fromUrl) {
+  const zipFilePath = `${RNFS.DocumentDirectoryPath}/model.zip`; // path where the downloaded zip file should be stored
+  const targetPath = `${RNFS.DocumentDirectoryPath}/model`; // path where the unzipped files should be stored
+
+  try {
+    const {jobId, promise} = RNFS.downloadFile({
+      fromUrl: fromUrl, // use the argument as the URL
+      toFile: zipFilePath,
+    });
+
+    await promise; // wait for the file to finish downloading
+    console.log(`download completed at ${zipFilePath}`);
+
+    await unzip(zipFilePath, targetPath); // unzip the downloaded file
+    console.log(`unzip completed at ${targetPath}`);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const InitialScene = props => {
+  const [rotation, setRotation] = useState(0);
+  const objectRef = useRef(null);
+
+  const [objectSource, setObjectSource] = useState(null);
+  const [resources, setResources] = useState([]);
+
+  useEffect(() => {
+    fetchAndUnzip().then(() => {
+      // Assuming the unzipped files are named as `model.obj` and `texture.jpg`
+      setObjectSource({
+        uri: `file://${RNFS.DocumentDirectoryPath}/model/model.obj`,
+      });
+      setResources([
+        {uri: `file://${RNFS.DocumentDirectoryPath}/model/texture.jpg`},
+      ]);
+    });
+  }, []);
+
+  function _onRotate(rotateState, rotationFactor, source) {
+    const scale = 0.1;
+    if (rotateState == 2) {
+      setRotation(prevRotation => prevRotation + rotationFactor * scale);
+    }
+  }
+
+  let data = props.sceneNavigator.viroAppProps;
+
+  return (
+    <ViroARScene>
+      <ViroAmbientLight color="#ffffff" />
+
+      {data.flag === true ? (
+        <Viro3DObject
+          //source={require('../assets/model3D/whiteChair/modern_chair11obj.obj')}
+          source={objectSource}
+          // resources={[
+          //   require('../assets/model3D/whiteChair/modern_chair11obj.mtl'),
+          //   require('../assets/model3D/whiteChair/0027.JPG'),
+          //   require('../assets/model3D/whiteChair/unrawpText.JPG'),
+          // ]}
+          resource={resources}
+          position={[0, -1, -1]}
+          scale={[0.02, 0.02, 0.02]}
+          type="OBJ"
+          onRotate={_onRotate}
+          // rotation={[0, rotation, 0]}
+          // dragType={'FixedToWorld'}
+
+          // dragType="FixedToPlane"
+          // dragPlane={{
+          //   planePoint: [0, -2, 0],
+          //   planeNormal: [0, 1, 0],
+          //   maxDistance: 4
+          // }}
+          // onDrag={() => { }}
+        />
+      ) : (
+        <ViroText
+          text={'Object is Hidden'}
+          scale={[0.5, 0.5, 0.5]}
+          position={[0, 0, -1]}
+        />
+      )}
+    </ViroARScene>
+  );
+};
 
 function ARScreen({navigation}) {
+  const [flag, setFlag] = useState(false);
+
   return (
-    <View>
-      <Text>AR Screen</Text>
+    <View style={styles.container}>
+      <ViroARSceneNavigator
+        initialScene={{scene: ReticleSceneAR}}
+        // initialScene={{ scene: InitialScene }}
+        viroAppProps={{flag: flag}}
+        style={{flex: 1}}
+      />
+
+      <View style={styles.controlsView}>
+        <TouchableOpacity style={styles.button} onPress={() => setFlag(!flag)}>
+          <Text style={styles.buttonText}>Reposition</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- app/screens/HelloWorldSceneAR.js
+++ app/screens/HelloWorldSceneAR.js
@@ -1,7 +1,7 @@
 'use strict';
 
-import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet} from 'react-native';
 import {
   ViroARScene,
   ViroText,
@@ -18,23 +18,39 @@ var createReactClass = require('create-react-class');
 var HelloWorldSceneAR = createReactClass({
   getInitialState() {
     return {
-      text: "Initializing AR..."
+      text: 'Initializing AR...',
     };
   },
 
   render: function () {
     return (
-      <ViroARScene onTrackingInitialized={() => { this.setState({ text: "Hello World!" }) }}>
-        <ViroText text={this.state.text} scale={[.1, .1, .1]} height={1} width={4} position={[0, .5, -1]} style={styles.helloWorldTextStyle} />
+      <ViroARScene
+        onTrackingInitialized={() => {
+          this.setState({text: 'Hello World!'});
+        }}>
+        <ViroText
+          text={this.state.text}
+          scale={[0.1, 0.1, 0.1]}
+          height={1}
+          width={4}
+          position={[0, 0.5, -1]}
+          style={styles.helloWorldTextStyle}
+        />
 
-        <ViroAmbientLight color={"#aaaaaa"} />
-        <ViroSpotLight innerAngle={5} outerAngle={90} direction={[0, -1, -.2]} position={[0, 3, 1]} color="#ffffff" castsShadow={true} />
+        <ViroAmbientLight color={'#aaaaaa'} />
+        <ViroSpotLight
+          innerAngle={5}
+          outerAngle={90}
+          direction={[0, -1, -0.2]}
+          position={[0, 3, 1]}
+          color="#ffffff"
+          castsShadow={true}
+        />
 
         <ViroNode
           position={[0, 0, -1]}
           dragType="FixedToWorld"
-          onDrag={() => { }}
-        >
+          onDrag={() => {}}>
           <Viro3DObject
             source={require('../assets/model3D/table/table.vrx')}
             position={[0, 0.1, 0]}
@@ -42,7 +58,6 @@ var HelloWorldSceneAR = createReactClass({
             type="VRX"
           />
         </ViroNode>
-
       </ViroARScene>
     );
   },
