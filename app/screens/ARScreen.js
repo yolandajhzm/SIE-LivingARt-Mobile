1afa12f1fe9a0dde5fbf9f35bfa0a540d1b2fb7c
yolandajhzm <jianghuizhuoma@gmail.com>
2023-08-07 15:26:04 -0700

    modelURL

--- app/screens/ARScreen.js
+++ app/screens/ARScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 function ARScreen({ route, navigation }) {
-    const model = route.params.model;
+    const modelURL = route.params.model;
     const dimension = route.params.dimension;
     console.log(model);
     console.log(dimension);
