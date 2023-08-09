2b41c8ded1b84d6bd0c53dc67a2a07205401353d
Yu Han <hanyu093@gmail.com>
2023-08-08 21:28:03 -0700

    Hide all warnings on the mobile screen

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavigationContainer from './app/navigation/NavigationContainer';
-
+import { LogBox } from "react-native"
+LogBox.ignoreAllLogs(true)
 export default function App() {
   return (
     <NavigationContainer />
